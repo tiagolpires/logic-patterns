0b7a295608762bfeefcabf0c756892cc0f50bb80
tiagolpires <tiagolpires@hotmail.com>
2022-05-02 10:07:25 -0300

    feat: add interface segregation principle

---
+++ solid/interfaceSegregation/script.js
@@ -0,0 +1,32 @@
+//Wrong
+//Turret implements an interface that has a method which Turret cannot implement
+
+// interface Entity {
+//     attackDamage
+//     health
+//     name
+
+//     move()
+//     attack()
+//     takeDamage(amount)
+// }
+
+// class Character implements Entity {
+//     move() {
+//         //Do something
+//     }
+
+//     attack() {
+//         //Do something
+//     }
+
+//     takeDamage(amount) {
+//         //Do something
+//     }
+// }
+
+// class Turret implements Entity {
+//     move() {
+//         //ERROR cannot move
+//     }
+// }
